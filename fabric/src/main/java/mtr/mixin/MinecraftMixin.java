12a308e705e9d15066faa07b98e5a9270722f9d6
Kenny-Hui <kenny.mh.hui@outlook.com>
2025-05-11 17:39:16 +0800

    Fix Fabric rendering injection point

--- fabric/src/main/java/mtr/mixin/MinecraftMixin.java
+++ fabric/src/main/java/mtr/mixin/MinecraftMixin.java
@@ -2,18 +2,17 @@ package mtr.mixin;
 
 import mtr.MTRClient;
 import mtr.render.RenderTrains;
-import net.minecraft.client.DeltaTracker;
-import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(GameRenderer.class)
-public class GameRendererMixin {
+@Mixin(Minecraft.class)
+public class MinecraftMixin {
 
-    @Inject(method = "render", at = @At("HEAD"))
-    void renderFramePre(DeltaTracker deltaTracker, boolean renderLevel, CallbackInfo ci) {
+    @Inject(method = "runTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GameRenderer;render(Lnet/minecraft/client/DeltaTracker;Z)V"))
+    void renderFramePre(CallbackInfo ci) {
         MTRClient.incrementGameTick();
         RenderTrains.simulate();
     }
